85c670d240d40e233ada1786e3b88d296e27ad65
eharinathkumar <harinathkumar@gmail.com>
2026-07-21 17:05:04 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "thalifit-v21";
+const CACHE = "thalifit-v22";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
