0b55d18c2df3d098209fa42739693025ae879dcc
eharinathkumar <harinathkumar@gmail.com>
2026-07-28 17:39:26 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "thalifit-v35";
+const CACHE = "thalifit-v37";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
